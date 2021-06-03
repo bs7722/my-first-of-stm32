c071dffaa76f1c17981e65d41ba6a6c10989d739
bs7722 <85263372+bs7722@users.noreply.github.com>
2021-06-03 09:37:12 +0800

    Update stm32LED.ino

--- stm32LED.ino
+++ stm32LED.ino
@@ -18,7 +18,7 @@
 //以上軟硬體環境：ACER Aspire V5-472g(notebook), OS:win10 64bit(20H2版)
 
 
-//STM32上pin 13(PC13)接的LED預設是LOW=1,HIGH=0
+//STM32上pin 13(PC13)接的LED預設是LOW=1,HIGH=0,LED顯示的電壓值與人類習慣性直觀不同，其原因與硬體有關，在此不多做解釋，懂的人自己想通，不懂的自己在上網查(pull-up/pull-down circuit)。
 #define pinLED PC13
 
 void setup() 
